a88fa32aeecc8cad32597f63064e6fd6a268de0e
Kate Anne Marie Dalangin <kitkatfdalangin@gmail.com>
2022-07-11 19:22:07 +0800

    New button to locate JSON File

--- DALANGIN.PDF-RESUME-CREATOR/Form2.Designer.cs
+++ DALANGIN.PDF-RESUME-CREATOR/Form2.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.choosefileButton = new System.Windows.Forms.Button();
+            this.datapreviewRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // choosefileButton
+            // 
+            this.choosefileButton.Location = new System.Drawing.Point(42, 51);
+            this.choosefileButton.Name = "choosefileButton";
+            this.choosefileButton.Size = new System.Drawing.Size(97, 44);
+            this.choosefileButton.TabIndex = 0;
+            this.choosefileButton.Text = "Choose a file";
+            this.choosefileButton.UseVisualStyleBackColor = true;
+            this.choosefileButton.Click += new System.EventHandler(this.choosefileButton_Click);
+            // 
+            // datapreviewRichTextBox
+            // 
+            this.datapreviewRichTextBox.Location = new System.Drawing.Point(42, 101);
+            this.datapreviewRichTextBox.Name = "datapreviewRichTextBox";
+            this.datapreviewRichTextBox.Size = new System.Drawing.Size(326, 293);
+            this.datapreviewRichTextBox.TabIndex = 1;
+            this.datapreviewRichTextBox.Text = "";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.datapreviewRichTextBox);
+            this.Controls.Add(this.choosefileButton);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button choosefileButton;
+        private System.Windows.Forms.RichTextBox datapreviewRichTextBox;
     }
 }
--- DALANGIN.PDF-RESUME-CREATOR/Form2.cs
+++ DALANGIN.PDF-RESUME-CREATOR/Form2.cs
@@ -16,5 +16,17 @@ namespace DALANGIN.PDF_RESUME_CREATOR
         {
             InitializeComponent();
         }
+
+        private void choosefileButton_Click(object sender, EventArgs e)
+        {
+            string jsonFile;
+            OpenFileDialog fileexplorer = new OpenFileDialog();
+
+            if (fileexplorer.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                jsonFile = fileexplorer.FileName;
+                datapreviewRichTextBox.Text = jsonFile;
+            }
+        }
     }
 }
